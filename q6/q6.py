b10efdd4ae48f4b097115506ded79bbe1d4101b5
Gazal Kaura <gazal.kaura22@gmail.com>
2021-08-15 12:14:47 +0800

    Fix q6

--- q6/q6.py
+++ q6/q6.py
@@ -11,19 +11,19 @@ def getDates(text):
         r'\d{2}/\d{2}/\d{4}': ['%d/%m/%Y','%m/%d/%Y'],
         r'\d{2} (?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sept|Oct|Nov|Dec) \d{4}': ['%d %b %Y']
     }
-    dates = {}
+    count = 0
     for k,v in pattern.items():
         match = re.findall(k, text)
-        try:
+        for m in match:
             for format in v:
-                for m in match:
+                try:
                     dt = datetime.strptime(m, format).date()
-                    dates[m] = 1
-        except:
-            pass
+                    count += 1
+                    break
+                except:
+                    pass
 
-    print(dates)
-    return len(dates)
+    return count
 
 text = open("sample.txt","r").read()
 print(getDates(text))
